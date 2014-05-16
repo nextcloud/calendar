b40a96edfe6ca2d78899045b5a4d343880a1d9c7
Georg Ehrke <developer@georgehrke.com>
2014-05-16 18:05:39 +0200

    update copyright header in backend classes

--- backend/local.php
+++ backend/local.php
@@ -1,11 +1,27 @@
 <?php
 /**
- * Copyright (c) 2014 Georg Ehrke <oc.list@georgehrke.com>
- * Copyright (c) 2014 Bart Visscher <bartv@thisnet.nl>
- * Copyright (c) 2014 Jakob Sack <mail@jakobsack.de>
- * This file is licensed under the Affero General Public License version 3 or
- * later.
- * See the COPYING-README file.
+ * ownCloud - Calendar App
+ *
+ * @author Georg Ehrke
+ * @copyright 2014 Georg Ehrke <oc.list@georgehrke.com>
+ * @author Bart Visscher
+ * @copyright 2014 Bart Visscher <bartv@thisnet.nl>
+ * @author Jakob Sack
+ * @copyright 2014 Jakob Sack <mail@jakobsack.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 namespace OCA\Calendar\Backend;
 
