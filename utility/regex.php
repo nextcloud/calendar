25c82c73796b06399aaa0914d4628d890a4a04f9
Georg Ehrke <developer@georgehrke.com>
2014-05-17 17:01:18 +0200

    add a few more regex to regexUtility

--- utility/regex.php
+++ utility/regex.php
@@ -17,8 +17,43 @@ class RegexUtility {
 
 
 	/**
-	 * regex for rgba
+	 * regex for rgb(a)
 	 * @var string
 	 */
 	const RGBA = '/#((?:[0-9a-fA-F]{2}){3}|(?:[0-9a-fA-F]{1}){3}|(?:[0-9a-fA-F]{1}){4}|(?:[0-9a-fA-F]{2}){4})$/';
+
+
+	/**
+	 * regex for VEvent
+	 * @var string
+	 */
+	const VEVENT = '/BEGIN:VEVENT\n([\s\S]*?)\nEND:VEVENT\n/';
+
+
+	/**
+	 * regex for VJournal
+	 * @var string
+	 */
+	const VJOURNAL = '/BEGIN:VJOURNAL\n([\s\S]*?)\nEND:VJOURNAL\n/';
+
+
+	/**
+	 * regex for VTodo
+	 * @var string
+	 */
+	const VTODO = '/BEGIN:VTODO\n([\s\S]*?)\nEND:VTODO\n/';
+
+
+	/**
+	 * regex for VFreeBusy
+	 * @var string
+	 */
+	const VFREEBUSY  = '/BEGIN:VFREEBUSY\n([\s\S]*?)\nEND:VFREEBUSY\n/';
+
+
+	/**
+	 * regex for VTimezone
+	 * @var string
+	 */
+	const VTIMEZONE = '/BEGIN:VTIMEZONE\n([\s\S]*?)\nEND:VTIMEZONE\n/';
 }
