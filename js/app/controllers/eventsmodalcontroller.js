acdca0f366824f740c383f4494311e7a97c329c8
raghunayyar <me@iraghu.com>
2014-07-16 03:37:25 +0530

    Removes Bogus Code and Initialises the Event Editor Dialog.

--- js/app/controllers/eventsmodalcontroller.js
+++ js/app/controllers/eventsmodalcontroller.js
@@ -22,64 +22,17 @@
  */
 app.controller('EventsModalController', ['$scope', '$routeParams', 'Restangular', 'CalendarModel', 'TimezoneModel', 'EventsModel', 'Model',
 	function ($scope, $routeParams, Restangular, CalendarModel, TimezoneModel, EventsModel, Model) {
+		
+		$scope.eventsmodel = EventsModel;
 
-		$scope.route = $routeParams;
-		var id = $scope.route.id;
-		$scope.calendars = CalendarModel.getAll();
-		$scope.timezones = TimezoneModel.getAll();
-		var eventResource = Restangular.one('calendars', id).one('events');
-		var timezoneResource = Restangular.one('timezones-list');
-		//var onetimezoneResource = Restangular.one('timezones', timezoneid);
-		//var timezone = onetimezoneResource.get(timezoneid);
-
-		// Initiates all models required to create a new calendar event
-		$scope.summary = '';
-		$scope.dtstart = '';
-		$scope.dtend = '';
-		$scope.location = '';
-		$scope.categories = '';
-		$scope.description = '';
-
-		$scope.repeatevents = [
-			{title: t('calendar', 'Daily'), id: 1 },
-			{title: t('calendar', 'Weekly'), id: 2 },
-			{title: t('calendar', 'Every Weekday'), id: 3 },
-			{title: t('calendar', 'Bi-weekly'), id: 4 },
-			{title: t('calendar', 'Monthly'), id: 5 },
-			{title: t('calendar', 'Yearly'), id: 6 }
-		];
-
-		$scope.endintervals = [
-			{title: t('calendar', 'Never'), id: 1 },
-			{title: t('calendar', 'by occurances'), id: 2 },
-			{title: t('calendar', 'by date'), id: 3 },
-		];
-
-		$scope.currenttz = TimezoneModel.currenttimezone().toUpperCase();
-		console.log($scope.currenttz);
-
-		$scope.create = function () {
-			var newevent = {
-				"currenttimezone": $scope.currenttz,
-				"uid": Model.uidgen,
-				"summary": $scope.summary,
-				"dtstart": $scope.dtstart,
-				"dtend": $scope.dtend,
-				"description": $scope.description,
-				"location": $scope.location,
-				"categories": $scope.categories,
-				"allday": $scope.allday
-				//"last-modified" : $scope.lastmodified
-				//"vtimezone" : TimezoneModel.getAll()
-			};
-			eventResource.post().then(function () {
-				console.log(newevent);
-				EventsModel.create(newevent);
-			});
-		};
-
-		timezoneResource.getList().then(function (timezones) {
-			TimezoneModel.addAll(timezones);
+		$scope.initDialog = angular.element('#event').dialog({
+			width : 500,
+			height: 600,
+			resizable: false,
+			draggable: false,
+			close: function(event, ui) {
+				$(this).dialog('destroy').remove();
+			}
 		});
 	}
 ]);
