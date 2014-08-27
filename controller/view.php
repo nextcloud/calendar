4c046192bfe6ea8484bc2f1d6c5b050a96e4b904
Georg Ehrke <developer@georgehrke.com>
2014-08-27 22:27:21 +0200

    move script/style includes from template to view controller

--- controller/view.php
+++ controller/view.php
@@ -4,6 +4,8 @@
  *
  * @author Georg Ehrke
  * @copyright 2014 Georg Ehrke <oc.list@georgehrke.com>
+ * @author Raghu Nayyar
+ * @copyright 2014 Raghu Nayyar <beingminimal@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
@@ -30,8 +32,36 @@ class ViewController extends Controller {
 	 * @NoCSRFRequired
 	 */
 	public function index(){
-		//$this->api->addStyle();
-		//$this->api->addScript();
+		$this->api->addScript('../3rdparty/js/fileupload/jquery.fileupload');
+		//$this->api->addScript('../3rdparty/js/recurrencejs/jquery.recurrenceinput');
+		$this->api->addScript('../3rdparty/js/timezones/jstz');
+		$this->api->addScript('../3rdparty/js/momentjs/moment.min');
+		$this->api->addScript('../3rdparty/js/fullcalendar/fullcalendar');
+		$this->api->addScript('../3rdparty/js/icaljs/ical');
+
+		$this->api->addScript('../3rdparty/js/angular/angular');
+		$this->api->addScript('../3rdparty/js/angular/angular-animate');
+		$this->api->addScript('../3rdparty/js/restangular/restangular');
+		$this->api->addScript('../3rdparty/js/angular/angular-route');
+
+		$this->api->addScript('../3rdparty/js/angular-ui/angular-ui');
+		$this->api->addScript('../3rdparty/js/angular-ui/angular-ui-calendar');
+		$this->api->addScript('../3rdparty/js/angular-ui/angular-ui-sortable');
+		$this->api->addScript('../3rdparty/js/colorpicker/colorpicker');
+		$this->api->addScript('../3rdparty/js/appframework/app');
+
+		$this->api->addScript('public/app');
+
+
+		$this->api->addStyle('calendar');
+		$this->api->addStyle('part.datepicker');
+		$this->api->addStyle('part.calendarlist');
+		$this->api->addStyle('part.settings');
+		$this->api->addStyle('part.events.dialog');
+
+		$this->api->addStyle('../3rdparty/css/fullcalendar/fullcalendar');
+		$this->api->addStyle('../3rdparty/css/colorpicker/colorpicker');
+
 		return new TemplateResponse('calendar', 'main');
 	}
 }
--- templates/main.php
+++ templates/main.php
@@ -22,40 +22,6 @@
  *
  */
 ?>
-
-<?php
-	\OCP\Util::addScript('calendar', '../3rdparty/js/fileupload/jquery.fileupload');
-	//\OCP\Util::addScript('calendar', '../3rdparty/js/recurrencejs/jquery.recurrenceinput');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/timezones/jstz');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/momentjs/moment.min');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/fullcalendar/fullcalendar');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/icaljs/ical');
-
-	\OCP\Util::addScript('calendar', '../3rdparty/js/angular/angular');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/angular/angular-animate');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/restangular/restangular');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/angular/angular-route');
-
-	\OCP\Util::addScript('calendar', '../3rdparty/js/angular-ui/angular-ui');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/angular-ui/angular-ui-calendar');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/angular-ui/angular-ui-sortable');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/colorpicker/colorpicker');
-	\OCP\Util::addScript('calendar', '../3rdparty/js/appframework/app');
-
-	\OCP\Util::addScript('calendar', 'public/app');
-
-
-	\OCP\Util::addStyle('calendar', 'calendar');
-	\OCP\Util::addStyle('calendar', 'part.datepicker');
-	\OCP\Util::addStyle('calendar', 'part.calendarlist');
-	\OCP\Util::addStyle('calendar', 'part.settings');
-	\OCP\Util::addStyle('calendar', 'part.events.dialog');
-
-	\OCP\Util::addStyle('calendar', '../3rdparty/css/fullcalendar/fullcalendar');
-	\OCP\Util::addStyle('calendar', '../3rdparty/css/colorpicker/colorpicker');
-
-?>
-
 <div ng-app="Calendar">
 
 	<script type="text/ng-template" id="calendar.html">
