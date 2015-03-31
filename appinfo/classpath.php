bb5912826c07c60e95077fbc014bbdc09d841b06
Georg Ehrke <developer@georgehrke.com>
2015-03-31 11:33:50 +0200

    cleanup utility folder

--- appinfo/classpath.php
+++ appinfo/classpath.php
@@ -63,16 +63,3 @@
 \OC::$CLASSPATH['OCA\Calendar\Sabre'] = '';
 \OC::$CLASSPATH['OCA\Calendar\Sabre'] = '';
 \OC::$CLASSPATH['OCA\Calendar\Sabre'] = '';*/
-
-
-/* utility classes */
-\OC::$CLASSPATH['OCA\Calendar\Utility\Utility'] = 'calendar/utility/utility.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\BackendUtility'] = 'calendar/utility/backend.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\CalendarUtility'] = 'calendar/utility/calendar.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\ColorUtility'] = 'calendar/utility/color.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\HookUtility'] = 'calendar/utility/hook.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\JSONUtility'] = 'calendar/utility/json.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\ObjectUtility'] = 'calendar/utility/object.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\RegexUtility'] = 'calendar/utility/regex.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\SabreUtility'] = 'calendar/utility/sabre.php';
-\OC::$CLASSPATH['OCA\Calendar\Utility\UpdateUtility'] = 'calendar/utility/update.php';
