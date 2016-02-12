b2a75a8a2b2b6f614644a1568991c49373819490
Georg Ehrke <developer@georgehrke.com>
2016-02-12 11:52:09 +0100

    rename from and to to starts and ends

--- templates/part.eventssidebareditor.php
+++ templates/part.eventssidebareditor.php
@@ -15,15 +15,15 @@
 
 		<fieldset class="advanced--fieldset">
 			<div class="event-time-interior pull-left pull-half">
-				<span><?php p($l->t('From')); ?></span>
-				<input type="text" name="from" id="advanced_from" ng-model="fromdatemodel" class="events--date" placeholder="<?php p($l->t('from'));?>" />
+				<span><?php p($l->t('starts')); ?></span>
+				<input type="text" name="from" id="advanced_from" ng-model="fromdatemodel" class="events--date" placeholder="<?php p($l->t('starts'));?>" />
 				<input type="text" name="fromtime" id="advanced_fromtime" ng-model="fromtimemodel" class="events--time" ng-disabled="properties.allDay" />
 				<select ng-options="timezone for timezone in timezones" ng-model="properties.dtstart.parameters.zone" ng-show="edittimezone" ng-disabled="properties.allDay"
 						ng-change="loadTimezone(properties.dtstart.parameters.zone)"></select>
 			</div>
 			<div class="event-time-interior pull-right pull-half">
-				<span><?php p($l->t('To')); ?></span>
-				<input type="text" name="to" id="advanced_to" ng-model="todatemodel" class="events--date" placeholder="<?php p($l->t('to'));?>" />
+				<span><?php p($l->t('ends')); ?></span>
+				<input type="text" name="to" id="advanced_to" ng-model="todatemodel" class="events--date" placeholder="<?php p($l->t('ends'));?>" />
 				<input type="text" name="totime" id="advanced_totime" ng-model="totimemodel" class="events--time" ng-disabled="properties.allDay" />
 				<select ng-options="timezone for timezone in timezones" ng-model="properties.dtend.parameters.zone" ng-show="edittimezone" ng-disabled="properties.allDay"
 					ng-change="loadTimezone(properties.dtend.parameters.zone)"></select>
