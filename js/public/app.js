508d6251d77a10407df19c7fc0be15320b8f5533
Georg Ehrke <developer@georgehrke.com>
2016-02-17 10:05:04 +0100

    fix calendar not disappearing in calendar editor after deletion

--- js/public/app.js
+++ js/public/app.js
@@ -474,6 +474,10 @@ app.controller('CalController', ['$scope', '$rootScope', '$window', 'CalendarSer
 		 * - delete event source object
 		 */
 		$rootScope.$on('removedCalendar', function (event, calendar) {
+			$scope.calendars = $scope.calendars.filter(function (element) {
+				return element.url !== calendar.url;
+			});
+
 			var deletedObject = calendar.url;
 			uiCalendarConfig.calendars.calendar.fullCalendar('removeEventSource',
 				$scope.eventSource[deletedObject]);
@@ -682,9 +686,6 @@ app.controller('CalendarListController', ['$scope', '$rootScope', '$window', 'Ca
 		$scope.remove = function (calendar) {
 			calendar.list.loading = true;
 			CalendarService.delete(calendar).then(function() {
-				$scope.calendars = $scope.calendars.filter(function (element) {
-					return element.url !== calendar.url;
-				});
 				$rootScope.$broadcast('removedCalendar', calendar);
 				$rootScope.$broadcast('reloadCalendarList');
 			});
