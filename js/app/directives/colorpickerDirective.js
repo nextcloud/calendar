eea2721bb0f6467f32aecd1e00e75ed3fc0f4e22
John Molakvoæ <skjnldsv@users.noreply.github.com>
2016-05-05 11:58:43 +0200

    New color generator - Update css for colour palette - Fixed tabs - Use core generator with specific values

--- js/app/directives/colorpickerDirective.js
+++ js/app/directives/colorpickerDirective.js
@@ -5,6 +5,7 @@
  * @author Georg Ehrke
  * @copyright 2016 Raghu Nayyar <beingminimal@gmail.com>
  * @copyright 2016 Georg Ehrke <oc.list@georgehrke.com>
+ * @copyright 2016 John Molakvoæ <fremulon@protonmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
@@ -13,48 +14,117 @@
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
  * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
  *
  * You should have received a copy of the GNU Affero General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  *
  */
+ 
+/* https://github.com/kayellpeee/hsl_rgb_converter
+ * expected hue range: [0, 360)
+ * expected saturation range: [0, 1]
+ * expected lightness range: [0, 1]
+ */
+var hslToRgb = function(hue, saturation, lightness) {
+	// based on algorithm from http://en.wikipedia.org/wiki/HSL_and_HSV#Converting_to_RGB
+	if (hue == undefined) {
+		return [0, 0, 0];
+	}
+
+	var chroma = (1 - Math.abs((2 * lightness) - 1)) * saturation;
+	var huePrime = hue / 60;
+	var secondComponent = chroma * (1 - Math.abs((huePrime % 2) - 1));
+
+	huePrime = Math.floor(huePrime);
+	var red;
+	var green;
+	var blue;
+
+	if (huePrime === 0) {
+		red = chroma;
+		green = secondComponent;
+		blue = 0;
+	} else if (huePrime === 1) {
+		red = secondComponent;
+		green = chroma;
+		blue = 0;
+	} else if (huePrime === 2) {
+		red = 0;
+		green = chroma;
+		blue = secondComponent;
+	} else if (huePrime === 3) {
+		red = 0;
+		green = secondComponent;
+		blue = chroma;
+	} else if (huePrime === 4) {
+		red = secondComponent;
+		green = 0;
+		blue = chroma;
+	} else if (huePrime === 5) {
+		red = chroma;
+		green = 0;
+		blue = secondComponent;
+	}
+
+	var lightnessAdjustment = lightness - (chroma / 2);
+	red += lightnessAdjustment;
+	green += lightnessAdjustment;
+	blue += lightnessAdjustment;
+
+	return [Math.round(red * 255), Math.round(green * 255), Math.round(blue * 255)];
+
+};
+
 
 /**
-* Directive: Colorpicker
-* Description: Colorpicker for the Calendar app.
-*/
+ * Directive: Colorpicker
+ * Description: Colorpicker for the Calendar app.
+ */
 
 
 app.directive('colorpicker', function() {
-  'use strict';
-    var listofcolours =  [
-        '#31CC7C',
-        '#317CCC',
-        '#FF7A66',
-        '#F1DB50',
-        '#7C31CC',
-        '#CC317C',
-        '#3A3B3D',
-        '#CACBCD'
-    ];
-    return {
-        scope: {
-            selected: '=',
-            customizedColors: '=colors'
-        },
-        restrict: 'AE',
-        templateUrl: OC.filePath('calendar','templates', 'colorpicker.html'),
-        link: function (scope, element, attr) {
-            scope.colors = scope.customizedColors || listofcolours;
-            scope.selected = scope.selected || scope.colors[0];
+	'use strict';
+	var listofcolours = [
+		'#31CC7C',
+		'#317CCC',
+		'#FF7A66',
+		'#F1DB50',
+		'#7C31CC',
+		'#CC317C',
+		'#3A3B3D',
+		'#CACBCD'
+	];
+	if (typeof String.prototype.toHsl === 'function') {
+		var hsl = "";
+		var hslcolour = "";
+		//		  0    40   80   120  160  200   240  280  320
+		listofcolours = ["15", "9", "4", "b", "6", "11", "e", "f", "57"];
+		listofcolours.forEach(function(hash, index) {
+			hsl = hash.toHsl();
+			hslcolour = hslToRgb(Math.round(hsl[0]/40)*40, hsl[1]/100, hsl[2]/100);
+			listofcolours[index] = '#' + parseInt(hslcolour[0], 10).toString(16)+
+										 parseInt(hslcolour[1], 10).toString(16)+
+										 parseInt(hslcolour[2], 10).toString(16)
+		});
+	}
+	return {
+		scope: {
+			selected: '=',
+			customizedColors: '=colors'
+		},
+		restrict: 'AE',
+		templateUrl: OC.filePath('calendar', 'templates', 'colorpicker.html'),
+		link: function(scope, element, attr) {
+			scope.colors = scope.customizedColors || listofcolours;
+			scope.selected = scope.selected || scope.colors[0];
 
-            scope.pick = function (color) {
-                scope.selected = color;
-            };
+			scope.pick = function(color) {
+				scope.selected = color;
+			};
 
-        }
-    };
+		}
+	};
 
 });
