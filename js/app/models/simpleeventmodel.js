a6a5a5a63a70d44e5e2736d77c53bbdd839262a6
Georg Ehrke <developer@georgehrke.com>
2016-08-12 20:54:45 +0200

    Revert "Fixed  tzid on floating date"
    This reverts commit 6e416a0c928a1c0d09830aa350fa64cd1dd9f13e.

--- js/app/models/simpleeventmodel.js
+++ js/app/models/simpleeventmodel.js
@@ -522,7 +522,8 @@ app.factory('SimpleEvent', function() {
 
 			var dtstart = new ICAL.Property('dtstart', vevent);
 			dtstart.setValue(start);
-			if (start.zone.tzid !== 'UTC' && start.zone.tzid !== 'floating') {
+		
+			if (start.zone.tzid !== 'UTC') {
 				dtstart.setParameter('tzid', newSimpleData.dtstart.parameters.zone);
 				
 				if (availableTimezones.indexOf(newSimpleData.dtstart.parameters.zone) === -1) {
@@ -533,7 +534,7 @@ app.factory('SimpleEvent', function() {
 
 			var dtend = new ICAL.Property('dtend', vevent);
 			dtend.setValue(end);
-			if (end.zone.tzid !== 'UTC' && end.zone.tzid !== 'floating') {
+			if (end.zone.tzid !== 'UTC') {
 				dtend.setParameter('tzid', newSimpleData.dtend.parameters.zone);
 				
 				if (availableTimezones.indexOf(newSimpleData.dtend.parameters.zone) === -1) {
