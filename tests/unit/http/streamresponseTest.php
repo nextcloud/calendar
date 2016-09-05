69a6b10ac093e271888a2711db6fe7b48643fbd7
Georg Ehrke <developer@georgehrke.com>
2016-09-05 12:48:18 +0200

    php unit tests

---
+++ tests/unit/http/streamresponseTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Calendar App
+ *
+ * @author Georg Ehrke
+ * @copyright 2016 Georg Ehrke <oc.list@georgehrke.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\Calendar\Http;
+
+class StreamResponseTest extends \PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+
+	}
+
+	public function testCallback() {
+		$stream = fopen('data://text/plain,test_data', 'r');
+		$this->expectOutputString('test_data');
+
+		$ioutput = $this->getMockBuilder('OCP\AppFramework\Http\IOutput')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$streamResponse = new StreamResponse($stream);
+		$streamResponse->callback($ioutput);
+	}
+
+	public function testCallbackRewind() {
+		$stream = fopen('data://text/plain,test_data', 'r');
+		fseek($stream, 5);
+		$this->expectOutputString('test_data');
+
+		$ioutput = $this->getMockBuilder('OCP\AppFramework\Http\IOutput')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$streamResponse = new StreamResponse($stream);
+		$streamResponse->callback($ioutput);
+	}
+}
