f0788b349c2c5cd45a2d5143fa57ce5b93259f2e
Thomas Citharel <tcit@tcit.fr>
2016-10-01 12:50:52 +0200

    show email for attendee

--- templates/part.eventsattendees.php
+++ templates/part.eventsattendees.php
@@ -25,7 +25,7 @@
 <div class="advanced--fieldset" ng-hide="emailAddress === ''">
 	<input type="text" class="advanced--input" ng-model="nameofattendee"
 		placeholder="<?php p($l->t('E-Mail address of attendee'))?>" name="nameofattendee" autocomplete="off"
-		uib-typeahead="contact.name for contact in search($viewValue)" typeahead-show-hint="true" typeahead-min-length="3"
+		uib-typeahead="contact as (contact.name | limitTo: 20) + ' (' + (contact.email | limitTo: 30) + ')' for contact in search($viewValue)" typeahead-show-hint="true" typeahead-min-length="3"
 		   typeahead-on-select="selectFromTypeahead($item)" />
 	<button id="addmoreattendees" ng-click="add(nameofattendee)" class="btn event-button button" type="button">
 		<?php p($l->t('Add')); ?>
