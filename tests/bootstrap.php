780a21a8cbc750bb87ba9b8f6490544848bbc105
Georg Ehrke <developer@georgehrke.com>
2017-02-18 19:16:28 +0100

    ugly hack to fix unit test
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,11 +12,15 @@
  */
 define('PHPUNIT_RUN', 1);
 
+// ugly hack to fix issues with template code using static code
+$_SERVER['REQUEST_URI'] = '/index.php/apps/calendar/';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 require_once __DIR__.'/../../../lib/base.php';
 
 if (version_compare(implode('.', \OCP\Util::getVersion()), '8.2', '>=')) {
 	\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
-	\OC_App::loadApp('mail');
+	\OC_App::loadApp('calendar');
 }
 
 if(!class_exists('PHPUnit_Framework_TestCase')) {
