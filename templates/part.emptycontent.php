a4781446e509a6a9651a12ee03fa0a2d48889865
Georg Ehrke <developer@georgehrke.com>
2017-03-28 13:41:02 +0200

    use dark calendar icon
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- templates/part.emptycontent.php
+++ templates/part.emptycontent.php
@@ -1,5 +1,5 @@
 <div id="emptycontent">
-	<div class="icon-calendar"></div>
+	<div class="icon-calendar-dark"></div>
 	<h2><?php p($l->t('Calendar does not exist')); ?></h2>
 	<p><?php p($l->t('Maybe you got a wrong link or the calendar was unshared?')); ?></p>
 </div>
