8fa7f4aa8a90821282a3412271fcd97d1d4d87a9
Georg Ehrke <developer@georgehrke.com>
2018-10-17 21:21:54 +0200

    add basic vue skeleton
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

---
+++ src/main.js
@@ -0,0 +1,53 @@
+/**
+ * @copyright Copyright (c) 2018 John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @author John Molakvoæ <skjnldsv@protonmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import '@babel/polyfill'
+
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+// import store from './store'
+// import { sync } from 'vuex-router-sync'
+
+// CSP config for webpack dynamic chunk loading
+// eslint-disable-next-line
+__webpack_nonce__ = btoa(OC.requestToken)
+
+// Correct the root of the app for chunk loading
+// OC.linkTo matches the apps folders
+// OC.generateUrl ensure the index.php (or not)
+// We do not want the index.php since we're loading files
+// eslint-disable-next-line
+__webpack_public_path__ = OC.linkTo('calendar', 'js/')
+
+// sync(store, router)
+
+Vue.prototype.t = t
+Vue.prototype.n = n
+Vue.prototype.OC = OC
+Vue.prototype.OCA = OCA
+
+export default new Vue({
+	el: '#content',
+	// router,
+	// store,
+	render: h => h(App)
+})
