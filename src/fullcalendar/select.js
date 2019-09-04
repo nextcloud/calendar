a9a118686e923f1dec6ede0a3d8680a552b7d965
Georg Ehrke <developer@georgehrke.com>
2019-09-04 12:10:34 +0200

    Use offical fullcalendar vue component
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

---
+++ src/fullcalendar/select.js
@@ -0,0 +1,42 @@
+/**
+ * @copyright Copyright (c) 2019 Georg Ehrke
+ *
+ * @author Georg Ehrke <oc.list@georgehrke.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ *
+ * @param {Object} store The Vuex store
+ * @param {Object} router The Vue router
+ * @returns {Function}
+ */
+export default function(store, router) {
+	return function({ start, end, allDay }) {
+		const name = store.state.settings.showPopover
+			? 'NewPopoverView'
+			: 'NewSidebarView'
+		const params = Object.assign({}, store.state.route.params, {
+			allDay: allDay ? '1' : '0',
+			dtstart: Math.floor(start.getTime() / 1000),
+			dtend: Math.floor(end.getTime() / 1000),
+		})
+
+		router.push({ name, params })
+	}
+}
