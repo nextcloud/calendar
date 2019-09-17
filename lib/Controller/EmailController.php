1a617c76b97afef385bf6f77f62f6968a7b6815a
Georg Ehrke <developer@georgehrke.com>
2019-09-17 19:04:35 +0200

    major clean up of EmailController, use email templates
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- lib/Controller/EmailController.php
+++ lib/Controller/EmailController.php
@@ -23,52 +23,50 @@ namespace OCA\Calendar\Controller;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\JSONResponse;
-use OCP\AppFramework\Http\TemplateResponse;
 use OCP\Defaults;
 use OCP\IConfig;
 use OCP\IL10N;
 use OCP\IRequest;
 use OCP\IUserSession;
+use OCP\Mail\IEMailTemplate;
 use OCP\Mail\IMailer;
+use OCP\Mail\IMessage;
 
 class EmailController extends Controller {
 
-	/**
-	 * @var IConfig
-	 */
+	/** @var IConfig */
 	private $config;
 
-	/**
-	 * @var Defaults
-	 */
+	/** @var Defaults */
 	private $defaults;
 
-	/**
-	 * @var IL10N
-	 */
+	/** @var IL10N */
 	private $l10n;
 
-	/**
-	 * @var IMailer
-	 */
+	/** @var IMailer */
 	private $mailer;
 
-	/**
-	 * @var IUserSession
-	 */
+	/** @var IUserSession */
 	private $userSession;
 
 	/**
-	 * @param string $appName
-	 * @param IRequest $request an instance of the request
+	 * EmailController constructor.
+	 *
+	 * @param $appName
+	 * @param IRequest $request
 	 * @param IUserSession $userSession
 	 * @param IConfig $config
 	 * @param IMailer $mailer
 	 * @param IL10N $l10N
 	 * @param Defaults $defaults
 	 */
-	public function __construct($appName, IRequest $request, IUserSession $userSession,
-								IConfig $config, IMailer $mailer, IL10N $l10N, Defaults $defaults) {
+	public function __construct(string $appName,
+								IRequest $request,
+								IUserSession $userSession,
+								IConfig $config,
+								IMailer $mailer,
+								IL10N $l10N,
+								Defaults $defaults) {
 		parent::__construct($appName, $request);
 		$this->config = $config;
 		$this->userSession = $userSession;
@@ -82,91 +80,105 @@ class EmailController extends Controller {
 	 * @param string $url
 	 * @param string $calendarName
 	 * @return JSONResponse
+	 *
 	 * @NoAdminRequired
 	 */
-	public function sendEmailPublicLink(string $recipient, string $url, string $calendarName):JSONResponse {
+	public function sendEmailPublicLink(string $recipient,
+										string $url,
+										string $calendarName):JSONResponse {
 		$user = $this->userSession->getUser();
-		$displayName = $user->getDisplayName();
+		if (!$user) {
+			return new JSONResponse([
+				'message' => $this->l10n->t('User-Session unexpectedly expired'),
+			], Http::STATUS_UNAUTHORIZED);
+		}
 
+		if (!$this->mailer->validateMailAddress($recipient)) {
+			return new JSONResponse([
+				'message' => $this->l10n->t('Provided email-address is not valid'),
+			], Http::STATUS_BAD_REQUEST);
+		}
+
+		$fromAddress = $this->getFromAddress();
+		$displayName = $user->getDisplayName();
 		$subject = $this->l10n->t('%s has published the calendar »%s«', [$displayName, $calendarName]);
 
-		$serverVersion = $this->config->getSystemValue('version');
-		if (version_compare($serverVersion, '12', '>=')) {
-			$emailTemplate = $this->mailer->createEMailTemplate('calendar.PublicShareNotification', [
-				'recipient' => $recipient,
-				'displayname' => $displayName,
-				'calendar_name' => $calendarName,
-				'calendar_url' => $url,
-			]);
-
-			$emailTemplate->addHeader();
-			$emailTemplate->addHeading($this->l10n->t('%s has published the calendar »%s«', [$displayName, $calendarName]));
-
-			$emailTemplate->addBodyText($this->l10n->t('Hello,'));
-			$emailTemplate->addBodyText($this->l10n->t('We wanted to inform you that %s has published the calendar »%s«.', [$displayName, $calendarName]));
-
-			$emailTemplate->addBodyButton($this->l10n->t('Open »%s«', [$calendarName]), $url);
-
-			// TRANSLATORS term at the end of a mail
-			$emailTemplate->addBodyText($this->l10n->t('Cheers!'));
-
-			$emailTemplate->addFooter();
-
-			$bodyHTML = $emailTemplate->renderHtml();
-			$textBody = $emailTemplate->renderText();
-		} else {
-			$emailTemplateHTML = new TemplateResponse('calendar', 'mail.publication.html', [
-				'subject' => $subject,
-				'username' => $displayName,
-				'calendarname' => $calendarName,
-				'calendarurl' => $url,
-				'defaults' => $this->defaults
-			], 'public');
-			$bodyHTML = $emailTemplateHTML->render();
-
-			$emailTemplateText = new TemplateResponse('calendar', 'mail.publication.text', [
-				'subject' => $subject,
-				'username' => $displayName,
-				'calendarname' => $calendarName,
-				'calendarurl' => $url
-			], 'blank');
-			$textBody = $emailTemplateText->render();
-		}
-
-		$status = $this->sendEmail($recipient, $subject, $bodyHTML, $textBody);
-
-		return new JSONResponse([], $status);
-	}
-
-	/**
-	 * @param string $recipient
-	 * @param string $subject
-	 * @param string $body
-	 * @param string $textBody
-	 * @return int
-	 */
-	private function sendEmail(string $recipient, string $subject, string $body, string $textBody):int {
-		if (!$this->mailer->validateMailAddress($recipient)) {
-			return Http::STATUS_BAD_REQUEST;
-		}
-
-		$sendFromDomain = $this->config->getSystemValue('mail_domain', 'domain.org');
-		$sendFromAddress = $this->config->getSystemValue('mail_from_address', 'nextcloud');
-		$sendFrom = $sendFromAddress . '@' . $sendFromDomain;
-
-		$message = $this->mailer->createMessage();
-		$message->setSubject($subject);
-		$message->setFrom([$sendFrom => $this->defaults->getName()]);
-		$message->setTo([$recipient => $this->l10n->t('Recipient')]);
-		$message->setPlainBody($textBody);
-		$message->setHtmlBody($body);
+		$template = $this->createTemplate($subject, $displayName, $calendarName, $url);
+		$message = $this->createMessage($fromAddress, [$recipient => $recipient], $template);
 
 		try {
 			$this->mailer->send($message);
 		} catch(\Exception $ex) {
-			return Http::STATUS_INTERNAL_SERVER_ERROR;
+			return new JSONResponse([
+				'message' => $this->l10n->t('Unexpected error sending email. Please contact your administrator.'),
+			]);
 		}
 
-		return Http::STATUS_OK;
+		return new JSONResponse([
+			'message' => $this->l10n->t('Successfully sent email to ' . $recipient),
+		]);
+	}
+
+	/**
+	 * Get the from address
+	 *
+	 * @return string
+	 */
+	private function getFromAddress():string {
+		$sendFromDomain = $this->config->getSystemValue('mail_domain', 'domain.org');
+		$sendFromAddress = $this->config->getSystemValue('mail_from_address', 'nextcloud');
+
+		return implode('@', [
+			$sendFromAddress,
+			$sendFromDomain
+		]);
+	}
+
+	/**
+	 * @param string $from
+	 * @param array $recipients
+	 * @param IEMailTemplate $template
+	 * @return IMessage
+	 */
+	private function createMessage(string $from,
+								   array $recipients,
+								   IEMailTemplate $template):IMessage {
+		$message = $this->mailer->createMessage();
+		$message->setFrom([$from => $this->defaults->getName()]);
+		$message->setTo($recipients);
+		$message->useTemplate($template);
+
+		return $message;
+	}
+
+	/**
+	 * @param string $subject
+	 * @param string $displayName
+	 * @param string $calendarName
+	 * @param string $url
+	 * @return IEMailTemplate
+	 */
+	private function createTemplate(string $subject,
+									string $displayName,
+									string $calendarName,
+									string $url):IEMailTemplate {
+		$emailTemplate = $this->mailer->createEMailTemplate('calendar.PublicShareNotification', [
+			'displayname' => $displayName,
+			'calendar_name' => $calendarName,
+			'calendar_url' => $url,
+		]);
+
+		$emailTemplate->setSubject($subject);
+
+		$emailTemplate->addHeader();
+		$emailTemplate->addHeading($this->l10n->t('%s has published the calendar »%s«', [$displayName, $calendarName]));
+		$emailTemplate->addBodyText($this->l10n->t('Hello,'));
+		$emailTemplate->addBodyText($this->l10n->t('We wanted to inform you that %s has published the calendar »%s«.', [$displayName, $calendarName]));
+		$emailTemplate->addBodyButton($this->l10n->t('Open »%s«', [$calendarName]), $url);
+		// TRANSLATORS term at the end of a mail
+		$emailTemplate->addBodyText($this->l10n->t('Cheers!'));
+		$emailTemplate->addFooter();
+
+		return $emailTemplate;
 	}
 }
