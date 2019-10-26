0d38f77ded3e8c25070950a745b8c891d55ec2e5
Georg Ehrke <developer@georgehrke.com>
2019-10-26 18:58:50 +0200

    Hide property info in read-only mode
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- src/mixins/PropertyMixin.js
+++ src/mixins/PropertyMixin.js
@@ -28,37 +28,87 @@
  */
 export default {
 	props: {
-		// This is coming from rfcProps
+		/**
+		 * The prop-model object containing information about the
+		 * displayed property. The information comes straight from
+		 * /src/models/rfcProps.js
+		 */
 		propModel: {
 			type: Object,
 			required: true
 		},
+		/**
+		 * An indicator whether or not the event is being viewed
+		 * in read-only mode
+		 */
 		isReadOnly: {
 			type: Boolean,
 			required: true
 		},
+		/**
+		 * The value to be displayed inside the property.
+		 * The type varies from property to property.
+		 */
 		value: {
 			required: true
 		}
 	},
 	computed: {
+		/**
+		 * Returns the icon stored in the property-model
+		 * If there is no icon set, it returns an empty string
+		 *
+		 * @returns {string}
+		 */
 		icon() {
 			return this.propModel.icon || ''
 		},
+		/**
+		 * Returns the placeholder text stored in the property-model
+		 * If there is no placeholder text set, it returns an empty string
+		 *
+		 * @returns {string}
+		 */
 		placeholder() {
 			return this.propModel.placeholder || ''
 		},
+		/**
+		 * Returns the placeholder text for tags stored in the property-model
+		 * If there is no placeholder text for tags set, it returns an empty string
+		 *
+		 * @returns {string}
+		 */
 		tagPlaceholder() {
 			return this.propModel.tagPlaceholder || ''
 		},
+		/**
+		 * Returns the info text stored in the property-model
+		 * If there is no info text set, it returns an empty string
+		 *
+		 * @returns {string}
+		 */
 		info() {
 			return this.propModel.info || ''
 		},
+		/**
+		 * Returns the property's readable name stored in the property-model
+		 * If there is no readable name set, it returns an empty string
+		 *
+		 * @returns {string}
+		 */
 		readableName() {
 			return this.propModel.readableName || ''
 		},
+		/**
+		 * Returns whether or not to display the info text.
+		 * The info text will only be displayed if there
+		 * is actually an info set and if the event is viewed
+		 * with edit permission.
+		 *
+		 * @returns {boolean}
+		 */
 		hasInfo() {
-			return this.propModel.info !== undefined
+			return this.propModel.info !== undefined && !this.isReadOnly
 		}
 	}
 }
