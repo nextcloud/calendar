7240c8b50f4e840aca313a2c731538f9063fb384
Georg Ehrke <developer@georgehrke.com>
2019-12-05 15:01:06 +0100

    Remove comments from final minified build
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,18 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: '#source-map'
+	mode: 'production',
+	devtool: '#source-map',
+	optimization: {
+		minimizer: [new TerserPlugin({
+			terserOptions: {
+				output: {
+					comments: false,
+				}
+			},
+			sourceMap: true,
+		})],
+	}
 })
