1a8a22b02e40364d3c12786553d9854c041d3442
JackyW <jackyw-83@hotmail.com>
2021-03-26 15:29:38 +0800

    fix: Ignore existing attendee without email in search results

--- src/components/Editor/Invitees/InviteesListSearch.vue
+++ src/components/Editor/Invitees/InviteesListSearch.vue
@@ -175,7 +175,7 @@ export default {
 						name = email
 					}
 
-					if (this.alreadyInvitedEmails.includes(email)) {
+					if (email && this.alreadyInvitedEmails.includes(email)) {
 						return
 					}
 
