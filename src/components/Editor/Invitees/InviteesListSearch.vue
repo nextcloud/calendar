1a1ceaf6832ba8aaaf2c4d26229992df87eefce2
JackyW <jackyw-83@hotmail.com>
2021-04-06 11:35:32 +0800

    Update InviteesListSearch.vue

--- src/components/Editor/Invitees/InviteesListSearch.vue
+++ src/components/Editor/Invitees/InviteesListSearch.vue
@@ -186,7 +186,7 @@ export default {
 						name = email
 					}
 
-					if (this.alreadyInvitedEmails.includes(email) && email) {
+					if (email && this.alreadyInvitedEmails.includes(email)) {
 						return
 					}
 
