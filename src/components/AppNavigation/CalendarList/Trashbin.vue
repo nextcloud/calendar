fcc43b2da7089e6016fc8507f938044e2553a9c3
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-06-02 09:39:16 +0200

    l10n: Remove "on"

--- src/components/AppNavigation/CalendarList/Trashbin.vue
+++ src/components/AppNavigation/CalendarList/Trashbin.vue
@@ -34,7 +34,7 @@
 						<tr>
 							<th>{{ t('calendar', 'Name') }}</th>
 							<th class="deletedAt">
-								{{ t('calendar', 'Deleted on') }}
+								{{ t('calendar', 'Deleted') }}
 							</th>
 							<th>&nbsp;</th>
 						</tr>
