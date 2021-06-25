70b9b5b2e944c462177368c0c75d9bec1124d879
Richard Steinmetz <richard@steinmetz.cloud>
2021-06-25 12:09:45 +0200

    Fix missing slashes in generated urls
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- src/services/talkService.js
+++ src/services/talkService.js
@@ -33,7 +33,7 @@ import { generateUrl, generateOcsUrl } from '@nextcloud/router'
 export async function createTalkRoom(eventTitle = null) {
 	let response
 	try {
-		response = await HTTPClient.post(generateOcsUrl('apps/spreed/api/v1', 2) + `room`, {
+		response = await HTTPClient.post(generateOcsUrl('apps/spreed/api/v1/', 2) + `room`, {
 			roomType: 3,
 			roomName: eventTitle || t('calendar', 'Chat room for event'),
 		})
@@ -44,6 +44,7 @@ export async function createTalkRoom(eventTitle = null) {
 		return generateURLForToken(token)
 	} catch (error) {
 		console.debug(error)
+		throw error
 	}
 }
 
