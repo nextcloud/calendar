a3b2ed258d1e6276dbac884273e63973319e58c6
Anna Larch <anna@nextcloud.com>
2021-11-23 18:39:40 +0100

    Add interval logic
    Signed-off-by: Anna Larch <anna@nextcloud.com>

--- lib/Service/Appointments/AvailabilityGenerator.php
+++ lib/Service/Appointments/AvailabilityGenerator.php
@@ -25,8 +25,15 @@ declare(strict_types=1);
 
 namespace OCA\Calendar\Service\Appointments;
 
+use DateInterval;
+use DatePeriod;
+use DateTime;
+use DateTimeImmutable;
+use DateTimeZone;
+use JsonException;
 use OCA\Calendar\Db\AppointmentConfig;
 use OCP\AppFramework\Utility\ITimeFactory;
+use RuntimeException;
 use function ceil;
 use function max;
 use function min;
@@ -78,16 +85,80 @@ class AvailabilityGenerator {
 			return [];
 		}
 
-		if ($config->getAvailability() === null) {
+		if (empty($config->getAvailability())) {
 			// No availability -> full time range is available
 			return [
 				new Interval($earliestStart, $latestEnd),
 			];
 		}
 
-		// TODO: derive intervals from RRULE
-		return [
-			new Interval($earliestStart, $latestEnd),
-		];
+		try {
+			$availabilityRule = json_decode($config->getAvailability(), true, 512, JSON_THROW_ON_ERROR);
+		} catch (JsonException $e) {
+			throw new RuntimeException('Could not parse JSON for slots', 0, $e);
+		}
+		$timeZone = $availabilityRule['timezoneId'];
+		$slots = $availabilityRule['slots'];
+
+		$applicableSlots = $this->filterDates($start, $slots, $timeZone);
+
+		$intervals = [];
+		foreach($applicableSlots as $slot) {
+			if($slot['end'] <= $earliestStart || $slot['start'] >= $latestEnd) {
+				continue;
+			}
+			$startSlot = max(
+				$earliestStart,
+				$slot['start']
+			);
+			$endSlot = min(
+				$latestEnd,
+				$slot['end']
+			);
+			$intervals[] = new Interval($startSlot, $endSlot);
+		}
+		return $intervals;
+	}
+
+	private function filterDates(int $start, array $availabilityArray, string $timeZone) : array {
+		// First, transform all timestamps to DateTime Objects
+		$availabilityRules = [];
+		foreach($availabilityArray as $key => $availabilitySlot) {
+			if(empty($availabilitySlot)) {
+				$availabilityRules[$key] = [];
+				continue;
+			}
+			$availabilityRules[$key] = [
+				'start' => (new DateTimeImmutable())->setTimestamp($availabilitySlot['start']),
+				'end' => (new DateTimeImmutable())->setTimestamp($availabilitySlot['end'])
+			];
+		}
+
+		// get the period the check can apply to
+		$tz = new DateTimeZone($timeZone);
+		$period = new DatePeriod(
+			(new DateTimeImmutable())->setTimestamp($start - 87600)->setTimezone($tz)->setTime(0,0),
+			new DateInterval('P1D'),
+			(new DateTimeImmutable())->setTimestamp($start + 87600)->setTimezone($tz)->setTime(23, 59),
+		);
+
+		$applicable = [];
+		/** @var DateTimeImmutable $item */
+		foreach($period as $item) {
+			// get the weekday from our item and select the applicable rule
+			$weekday = strtoupper(mb_strcut($item->format('D'), 0, 2));
+			/** @var DateTimeImmutable[] $dailyRule */
+			$dailyRule = $availabilityRules[$weekday];
+			// days with no rule should be treated as unavailable
+			if(empty($dailyRule)) {
+				continue;
+			}
+			$dStart = $dailyRule['start'];
+			$applicable[$weekday]['start'] = $item->setTime((int)$dStart->format('H'), (int)$dStart->format('i'))->getTimestamp();
+			$dEnd = $dailyRule['end'];
+			$applicable[$weekday]['end'] = $item->setTime((int)$dEnd->format('H'), (int)$dEnd->format('i'))->getTimestamp();
+		}
+		// we now have an array that contains filtered and current timestamps for the availability
+		return $applicable;
 	}
 }
