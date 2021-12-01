a9c623b89f03305ed458fc823bab72f35ab7023c
Anna Larch <anna@nextcloud.com>
2021-12-01 16:16:24 +0100

    Check for TRANSP and CANCELLED property when conflict checking
    Signed-off-by: Anna Larch <anna@nextcloud.com>

--- lib/Service/Appointments/EventConflictFilter.php
+++ lib/Service/Appointments/EventConflictFilter.php
@@ -26,17 +26,22 @@ declare(strict_types=1);
 namespace OCA\Calendar\Service\Appointments;
 
 use DateInterval;
+use DateTimeInterface;
 use OCA\Calendar\Db\AppointmentConfig;
 use OCP\Calendar\IManager;
+use Psr\Log\LoggerInterface;
 use function array_filter;
 
 class EventConflictFilter {
 
 	/** @var IManager */
 	private $calendarManager;
+	private $logger;
 
-	public function __construct(IManager $calendarManager) {
+	public function __construct(IManager $calendarManager,
+					LoggerInterface $logger) {
 		$this->calendarManager = $calendarManager;
+		$this->logger = $logger;
 	}
 
 	/**
@@ -56,14 +61,31 @@ class EventConflictFilter {
 		}
 		// Always check the target calendar for conflicts
 		$query->addSearchCalendar($config->getTargetCalendarUri());
+		$query->addType('VEVENT');
 		$preparationDuration = DateInterval::createFromDateString($config->getPreparationDuration() . ' seconds');
 		$followUpDuration = DateInterval::createFromDateString($config->getFollowupDuration() . ' seconds');
-
-		return array_filter($slots, function (Interval $slot) use ($followUpDuration, $preparationDuration, $query): bool {
+		return array_filter($slots, function (Interval $slot) use ($followUpDuration, $preparationDuration, $query, $config): bool {
 			$query->setTimerangeStart($slot->getStartAsObject()->sub($preparationDuration));
 			$query->setTimerangeEnd($slot->getEndAsObject()->add($followUpDuration));
 
-			$objects = $this->calendarManager->searchForPrincipal($query);
+			$events = $this->calendarManager->searchForPrincipal($query);
+
+			$objects = array_filter($events, static function (array $event) {
+				$isCancelled = ($event['objects'][0]['STATUS'][0] ?? null) === 'CANCELLED';
+				$isFree = ($event['objects'][0]['TRANSP'][0] ?? null) === 'TRANSPARENT';
+				return !$isCancelled && !$isFree;
+			});
+
+			$uids = array_map(static function ($object) {
+				return $object['uid'];
+			}, $objects);
+
+			$this->logger->debug('Appointment config ' . $config->getToken() . ' is looking within {start} and {followup} in calendar {calendarUri}. Conflicting UIDs are {uids}', [
+				'start' => $slot->getStartAsObject()->sub($preparationDuration)->format(DateTimeInterface::ATOM),
+				'end' => $slot->getEndAsObject()->add($followUpDuration)->format(DateTimeInterface::ATOM),
+				'calendarUri' => $config->getTargetCalendarUri(),
+				'uids' => implode(' : ', $uids)
+			]);
 
 			// If there is at least one event at this time then the slot is taken
 			return empty($objects);
