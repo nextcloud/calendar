5237a42fc1b95b43a717b07a2bed59d2c1c25651
julia.kirschenheuter <julia.kirschenheuter@nextcloud.com>
2022-06-01 16:35:51 +0200

    Move event rendering to vue
    Fix tests
    Fix styles
    Fix linter
    
    Signed-off-by: julia.kirschenheuter <julia.kirschenheuter@nextcloud.com>

---
+++ src/components/EventRender.vue
@@ -0,0 +1,74 @@
+<!--
+  - @copyright Copyright (c) 2022 Thomas Citharel <nextcloud@tcit.fr>
+  -
+  - @author Thomas Citharel <nextcloud@tcit.fr>
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+<template>
+	<div>
+		<div class="fc-event-main-frame">
+			<div class="fc-event-time">
+				{{ eventDetails.timeText }}
+			</div>
+			<div class="fc-event-title-container fc-event-main-frame--icons">
+				<div class="fc-event-title fc-sticky">
+					<span class="fc-event-title">{{ eventDetails.event.title }}</span>
+				</div>
+				<Bell v-if="hasAlarms"
+					class="icon-event-reminder"
+					:size="14"
+					:style="{ color: isDarkText ? 'var(--color-main-text)' : 'var(--fc-event-text-color)' }" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Bell from 'vue-material-design-icons/Bell.vue'
+
+export default {
+	name: 'EventRender',
+	components: {
+		Bell,
+	},
+	props: {
+		eventDetails: {
+			type: Object,
+			required: true,
+		},
+	},
+	computed: {
+		viewType() {
+			return this.eventDetails?.view?.type
+		},
+		hasAlarms() {
+			return this.eventDetails?.event?._def?.extendedProps?.hasAlarms
+		},
+		isDarkText() {
+			return this.eventDetails?.event?._def?.extendedProps?.darkText
+		},
+	},
+}
+</script>
+
+<style scoped>
+.fc-event-main-frame--icons {
+	display: flex;
+	justify-content: space-between;
+}
+</style>
