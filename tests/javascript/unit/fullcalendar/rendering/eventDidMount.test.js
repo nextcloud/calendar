ca2abbb2e82a753bcd6b5bbf34561b862819c955
Christoph Wurst <christoph@winzerhof-wurst.at>
2022-07-06 10:35:20 +0200

    Revert "Move event rendering to vue"
    This reverts commit 5237a42fc1b95b43a717b07a2bed59d2c1c25651.

--- tests/javascript/unit/fullcalendar/rendering/eventDidMount.test.js
+++ tests/javascript/unit/fullcalendar/rendering/eventDidMount.test.js
@@ -19,7 +19,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-import eventRender from '../../../../../src/fullcalendar/rendering/eventDidMount.js'
+import eventRender from "../../../../../src/fullcalendar/rendering/eventDidMount.js";
 
 describe('fullcalendar/eventDidMount test suite', () => {
 
@@ -82,6 +82,8 @@ describe('fullcalendar/eventDidMount test suite', () => {
 		}
 
 		eventRender({ event, el })
+
+		expect(el.outerHTML).toEqual('<div class="fc-event-nc-alarms" data-object-id="object123" data-recurrence-id="recurrence456"><div class="fc-content"><span class="fc-time">2pm</span><span class="fc-title">Title 123</span><span class="icon-event-reminder icon-event-reminder--dark"></span></div></div>')
 	})
 
 	it('should add an alarm bell icon if event has an alarm - light', () => {
@@ -112,6 +114,73 @@ describe('fullcalendar/eventDidMount test suite', () => {
 		}
 
 		eventRender({ event, el })
+
+		expect(el.outerHTML).toEqual('<div class="fc-event-nc-alarms" data-object-id="object123" data-recurrence-id="recurrence456"><div class="fc-content"><span class="fc-time">2pm</span><span class="fc-title">Title 123</span><span class="icon-event-reminder icon-event-reminder--light"></span></div></div>')
 	})
+
+	// TODO: fix me later
+	// it('should prepend a checkbox before tasks - incomplete', () => {
+	// 	const fcTime = document.createElement('span')
+	// 	fcTime.classList.add('fc-time')
+	// 	fcTime.appendChild(document.createTextNode('2pm'))
+	// 	const fcTitle = document.createElement('span')
+	// 	fcTitle.classList.add('fc-title')
+	// 	fcTitle.appendChild(document.createTextNode('Title 123'))
+	//
+	// 	const fcContent = document.createElement('div')
+	// 	fcContent.classList.add('fc-content')
+	// 	fcContent.appendChild(fcTime)
+	// 	fcContent.appendChild(fcTitle)
+	//
+	// 	const el = document.createElement('div')
+	// 	el.classList.add('fc-event-nc-task')
+	// 	el.appendChild(fcContent)
+	//
+	// 	const event = {
+	// 		source: {},
+	// 		extendedProps: {
+	// 			objectId: 'object123',
+	// 			recurrenceId: 'recurrence456',
+	// 			darkText: false,
+	// 			percent: 50,
+	// 		},
+	// 	}
+	//
+	// 	eventRender({ event, el })
+	//
+	// 	expect(el.outerHTML).toEqual('<div class="fc-event-nc-task" data-object-id="object123" data-recurrence-id="recurrence456"><div class="fc-content"><span class="icon-event-task icon-event-task--light"></span><span class="fc-time">2pm</span><span class="fc-title">Title 123</span></div></div>')
+	// })
+	//
+	// it('should prepend a checkbox before tasks - completed', () => {
+	// 	const fcTime = document.createElement('span')
+	// 	fcTime.classList.add('fc-time')
+	// 	fcTime.appendChild(document.createTextNode('2pm'))
+	// 	const fcTitle = document.createElement('span')
+	// 	fcTitle.classList.add('fc-title')
+	// 	fcTitle.appendChild(document.createTextNode('Title 123'))
+	//
+	// 	const fcContent = document.createElement('div')
+	// 	fcContent.classList.add('fc-content')
+	// 	fcContent.appendChild(fcTime)
+	// 	fcContent.appendChild(fcTitle)
+	//
+	// 	const el = document.createElement('div')
+	// 	el.classList.add('fc-event-nc-task')
+	// 	el.appendChild(fcContent)
+	//
+	// 	const event = {
+	// 		source: {},
+	// 		extendedProps: {
+	// 			objectId: 'object123',
+	// 			recurrenceId: 'recurrence456',
+	// 			darkText: false,
+	// 			percent: 100,
+	// 		},
+	// 	}
+	//
+	// 	eventRender({ event, el })
+	//
+	// 	expect(el.outerHTML).toEqual('<div class="fc-event-nc-task" data-object-id="object123" data-recurrence-id="recurrence456"><div class="fc-content"><span class="icon-event-task icon-event-task--light icon-event-task--checked--light"></span><span class="fc-time">2pm</span><span class="fc-title">Title 123</span></div></div>')
+	// })
 
 })
