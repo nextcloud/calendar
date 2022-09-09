c4968f68c581ea5104676c45e6d2b102dbddedea
Richard Steinmetz <richard@steinmetz.cloud>
2022-09-09 12:11:43 +0200

    Replace license headers with SPDX identifiers
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- src/components/AppNavigation/CalendarList/CalendarListItemSharingSearch.vue
+++ src/components/AppNavigation/CalendarList/CalendarListItemSharingSearch.vue
@@ -5,7 +5,7 @@
   - @author Georg Ehrke <oc.list@georgehrke.com>
   - @author Jakob Röhrl <jakob.roehrl@web.de>
   -
-  - @license GNU AGPL version 3 or any later version
+  - @license AGPL-3.0-or-later
   -
   - This program is free software: you can redistribute it and/or modify
   - it under the terms of the GNU Affero General Public License as
--- src/views/EditSidebar.vue
+++ src/views/EditSidebar.vue
@@ -6,7 +6,7 @@
   - @author Jakob Röhrl <jakob.roehrl@web.de>
   - @author Richard Steinmetz <richard@steinmetz.cloud>
   -
-  - @license GNU AGPL version 3 or any later version
+  - @license AGPL-3.0-or-later
   -
   - This program is free software: you can redistribute it and/or modify
   - it under the terms of the GNU Affero General Public License as
