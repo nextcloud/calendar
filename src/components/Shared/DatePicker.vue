d10ed2fee69f0a1a6ccba2fa5d6db59b28e8b985
Richard Steinmetz <richard@steinmetz.cloud>
2023-04-11 12:14:36 +0200

    Revert "fix: date picker formatting invalid week numbers"
    This reverts commit 53cf255db29c33392f09b13591545726f9d2f4c4.
    A proper fix is now included in the @nextcloud/vue bump.
    
    Ref #5111
    
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- src/components/Shared/DatePicker.vue
+++ src/components/Shared/DatePicker.vue
@@ -2,7 +2,6 @@
   - @copyright Copyright (c) 2019 Georg Ehrke <oc.list@georgehrke.com>
   -
   - @author Georg Ehrke <oc.list@georgehrke.com>
-  - @author Richard Steinmetz <richard@steinmetz.cloud>
   -
   - @license AGPL-3.0-or-later
   -
@@ -148,6 +147,10 @@ export default {
 		return {
 			firstDay: getFirstDay() === 0 ? 7 : getFirstDay(),
 			showTimezonePopover: false,
+			formatter: {
+				stringify: this.stringify,
+				parse: this.parse,
+			},
 			showTimePanel: true,
 		}
 	},
@@ -217,23 +220,6 @@ export default {
 			const timeFormat = localeData.longDateFormat('LT').toLowerCase()
 
 			return timeFormat.indexOf('a') !== -1
-		},
-		formatter() {
-			// Wrap moment constructor as this.locale is reactive
-			const momentFactory = (date) => moment(date).locale(this.locale)
-
-			return {
-				stringify: this.stringify,
-				parse: this.parse,
-
-				// Overwrite week-of-year formatting as it depends on the locale
-				// FIXME: remove this workaround once the upstream PR is merged and released
-				//        https://github.com/nextcloud/nextcloud-vue/pull/3941
-				// FIXME: the formatter config can also be moved to data() again (as it is static)
-				getWeek(date) {
-					return momentFactory(date).format('w')
-				}
-			}
 		},
 	},
 	methods: {
