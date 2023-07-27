8820948844096cf165d41abef315f07dea91eccd
Christoph Wurst <christoph@winzerhof-wurst.at>
2023-07-27 14:48:09 +0200

    feat(talk): Make event attendees Talk room participants
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/services/talkService.js
+++ src/services/talkService.js
@@ -25,16 +25,20 @@ import HTTPClient from '@nextcloud/axios'
 import { translate as t } from '@nextcloud/l10n'
 import { generateUrl, generateOcsUrl } from '@nextcloud/router'
 import { loadState } from '@nextcloud/initial-state'
+import { getCurrentUser } from '@nextcloud/auth'
+import logger from '../utils/logger.js'
+import { removeMailtoPrefix } from '../utils/attendee.js'
 
 /**
  * Creates a new public talk room
  *
  * @param {?string} eventTitle Title of the event
  * @param {?string} eventDescription Description of the event
+ * @param {?string[]} attendees Attendees of the event
  *
  * @return {Promise<string>}
  */
-export async function createTalkRoom(eventTitle = null, eventDescription = null) {
+export async function createTalkRoom(eventTitle = null, eventDescription = null, attendees = []) {
 	const apiVersion = loadState('calendar', 'talk_api_version')
 	try {
 		const response = await HTTPClient.post(generateOcsUrl('apps/spreed/api/' + apiVersion + '/', 2) + 'room', {
@@ -54,6 +58,66 @@ export async function createTalkRoom(eventTitle = null, eventDescription = null)
 	} catch (error) {
 		console.debug(error)
 		throw error
+	}
+}
+
+/**
+ *
+ * @param eventComponent
+ */
+export async function updateTalkParticipants(eventComponent) {
+	const apiVersion = loadState('calendar', 'talk_api_version')
+	const url = eventComponent.getConferenceList()[0]?.uri ?? eventComponent.location
+	if (!url || !url.startsWith(window.location.protocol + '//' + window.location.host)) {
+		logger.debug('Event\'s conference/location is from another host', url)
+		return
+	}
+	const token = url.match(/\/call\/([a-z0-9]*)$/)[1]
+	if (!token) {
+		logger.debug('URL ' + url + ' contains no call token')
+		return
+	}
+	try {
+		const participantsResponse = await HTTPClient.get(generateOcsUrl('apps/spreed/api/' + apiVersion + '/', 2) + 'room/' + token + '/participants')
+		// Ignore if the actor isn't owner of the conversation
+		if (!participantsResponse.data.ocs.data.some(participant => participant.actorId === getCurrentUser().uid && participant.participantType <= 2)) {
+			logger.debug('Current user is not a moderator or owner', { currentUser: getCurrentUser().uid, conversation: participantsResponse.data.ocs.data })
+			return
+		}
+
+		for (const attendee of eventComponent.getAttendeeIterator()) {
+			logger.debug('Processing attendee', { attendee })
+			if (['GROUP', 'RESOURCE', 'ROOM'].includes(attendee.userType)) {
+				continue
+			}
+
+			let participantId = removeMailtoPrefix(attendee.email)
+			let attendeeSource = 'emails'
+			try {
+				// Map attendee email to Nextcloud user uid
+				const searchResult = await HTTPClient.get(generateOcsUrl('core/autocomplete/', 2) + 'get?search=' + encodeURIComponent(participantId) + '&itemType=&itemId=%20&shareTypes[]=0&limit=2')
+				// Only map if there is exactly one result. Use email if there are none or more results.
+				if (searchResult.data.ocs.data.length === 1) {
+					participantId = searchResult.data.ocs.data[0].id
+					attendeeSource = 'users'
+				} else {
+					logger.debug('Attendee ' + participantId + ' is not a Nextcloud user')
+				}
+			} catch (error) {
+				logger.info('Could not find user data for attendee ' + participantId, { error })
+			}
+
+			if (attendeeSource === 'users' && participantId === getCurrentUser().uid) {
+				logger.debug('Skipping organizer')
+				continue
+			}
+			await HTTPClient.post(generateOcsUrl('apps/spreed/api/' + apiVersion + '/', 2) + 'room/' + token + '/participants', {
+				newParticipant: participantId,
+				source: attendeeSource,
+			})
+		}
+	} catch (error) {
+		logger.warn('Could not update Talk room attendees', { error })
 	}
 }
 
