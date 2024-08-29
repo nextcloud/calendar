d2b4645a114648fdfeb2bf49ddb031bbca994a4f
Grigory Vodyanov <scratchx@gmx.com>
2024-08-29 17:03:53 +0200

    fix(attachmentService, propfindErrorParse): add an error message for when a file is not compatible for windows
    Signed-off-by: Grigory Vodyanov <scratchx@gmx.com>

---
+++ src/utils/propfindErrorParse.js
@@ -0,0 +1,28 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { t } from '@nextcloud/l10n'
+
+/**
+ * Parse PROPFIND error when uploading a file and return a readable message.
+ *
+ * @param exception the error object
+ */
+async function parseUploadError(exception) {
+	try {
+		const responseText = await exception.response.data
+		const parser = new DOMParser()
+		const xmlDoc = parser.parseFromString(responseText, 'application/xml')
+		const messageElement = xmlDoc.getElementsByTagName('s:message')[0]
+
+		return messageElement?.textContent
+	} catch (parseError) {
+		console.error(t('spreed', 'Error while parsing a PROPFIND error'), parseError)
+	}
+}
+
+export {
+	parseUploadError,
+}
