65d399d2bbb1dfb3eeaefb57813a2e4e08111afa
Grigory Vodyanov <scratchx@gmx.com>
2025-04-04 16:11:34 +0200

    fix(EditorMixin): add viewed by organizer if no attendees

--- src/mixins/EditorMixin.js
+++ src/mixins/EditorMixin.js
@@ -222,6 +222,10 @@ export default {
 		 * @return {boolean|null}
 		 */
 		isViewedByOrganizer() {
+			if (!this.calendarObjectInstance.attendees.length) {
+				return true
+			}
+
 			if (this.isReadOnly || !this.principalsStore.getCurrentUserPrincipalEmail || !this.calendarObjectInstance.organizer) {
 				return null
 			}
