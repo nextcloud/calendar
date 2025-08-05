e1225aecb5249f382213f2c8a79fac05d44e08a1
Hamza <hamzamahjoubi221@gmail.com>
2025-08-05 14:14:36 +0200

    chore(styleint): make rtl checks error level
    Signed-off-by: Hamza <hamzamahjoubi221@gmail.com>

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,33 @@
  * SPDX-FileCopyrightText: 2021 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-const stylelintConfig = require('@nextcloud/stylelint-config')
 
-module.exports = stylelintConfig
+module.exports = {
+	extends: '@nextcloud/stylelint-config',
+	plugins: ['stylelint-use-logical'],
+	// TODO: Remove this when the Nextcloud stylelint config is updated to include this rule (planned)
+	rules: {
+		'csstools/use-logical': [
+			'always',
+			{
+				severity: 'error',
+				// Only lint LTR-RTL properties for now
+				except: [
+					// Position properties
+					'top',
+					'bottom',
+					// Position properties with directional suffixes
+					/-top$/,
+					/-bottom$/,
+					// Size properties
+					'width',
+					'max-width',
+					'min-width',
+					'height',
+					'max-height',
+					'min-height',
+				],
+			},
+		],
+	},
+}
