4e0dc7d3de96a236156c8781d0078d4cf8ddb501
SebastianKrupinski <krupinskis05@gmail.com>
2025-08-23 11:36:41 -0400

    chore(feat): move calendar setting to modal
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- src/components/AppNavigation/Settings/ShortcutOverview.vue
+++ src/components/AppNavigation/Settings/ShortcutOverview.vue
@@ -3,52 +3,43 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
-	<NcDialog size="large"
-		:name="$t('calendar', 'Shortcut overview')"
-		:close-on-click-outside="true"
-		@update:open="onUpdateOpen">
-		<div class="shortcut-overview-modal">
-			<section v-for="category in shortcuts"
-				:key="category.categoryId"
-				class="shortcut-section">
-				<h3 class="shortcut-section__header">
-					{{ category.categoryLabel }}
-				</h3>
-				<div v-for="(shortcut, index) in category.shortcuts"
-					:key="`${category.categoryId}-${index}`"
-					class="shortcut-section-item">
-					<span class="shortcut-section-item__keys">
-						<template v-for="(keyCombination, index2) of shortcut.keys">
-							<template v-for="(key, index3) in keyCombination">
-								<kbd :key="`${category.categoryId}-${index}-${index2}-${index3}`">{{ key }}</kbd>
-								<span v-if="index3 !== (keyCombination.length - 1)"
-									:key="`${category.categoryId}-${index}-${index2}-${index3}`"
-									class="shortcut-section-item__spacer">
-									+
-								</span>
-							</template>
-							<span v-if="index2 !== (shortcut.keys.length - 1)"
-								:key="`${category.categoryId}-${index}-${index2}`"
+	<div class="shortcut-overview-modal">
+		<section v-for="category in shortcuts"
+			:key="category.categoryId"
+			class="shortcut-section">
+			<h3 class="shortcut-section__header">
+				{{ category.categoryLabel }}
+			</h3>
+			<div v-for="(shortcut, index) in category.shortcuts"
+				:key="`${category.categoryId}-${index}`"
+				class="shortcut-section-item">
+				<span class="shortcut-section-item__keys">
+					<template v-for="(keyCombination, index2) of shortcut.keys">
+						<template v-for="(key, index3) in keyCombination">
+							<kbd :key="`${category.categoryId}-${index}-${index2}-${index3}`">{{ key }}</kbd>
+							<span v-if="index3 !== (keyCombination.length - 1)"
+								:key="`${category.categoryId}-${index}-${index2}-${index3}`"
 								class="shortcut-section-item__spacer">
-								{{ $t('calendar', 'or') }}
+								+
 							</span>
 						</template>
-					</span>
-					<span class="shortcut-section-item__label">{{ shortcut.label }}</span>
-				</div>
-			</section>
-		</div>
-	</NcDialog>
+						<span v-if="index2 !== (shortcut.keys.length - 1)"
+							:key="`${category.categoryId}-${index}-${index2}`"
+							class="shortcut-section-item__spacer">
+							{{ $t('calendar', 'or') }}
+						</span>
+					</template>
+				</span>
+				<span class="shortcut-section-item__label">{{ shortcut.label }}</span>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
 import { translate as t } from '@nextcloud/l10n'
-import { NcDialog } from '@nextcloud/vue'
 
 export default {
-	components: {
-		NcDialog,
-	},
 	computed: {
 		shortcuts() {
 			return [{
@@ -112,54 +103,44 @@ export default {
 			}]
 		},
 	},
-	methods: {
-		onUpdateOpen(open) {
-			if (!open) {
-				this.$emit('close')
-			}
-		},
-	},
 }
 </script>
 
 <style lang="scss" scoped>
-.shortcut-overview-modal {
-	display: flex;
-	flex-wrap: wrap;
 
-	.shortcut-section {
-		width: 50%;
-		min-width: 425px;
-		flex-grow: 0;
-		flex-shrink: 0;
-		padding: 10px;
-		box-sizing: border-box;
+.shortcut-section {
+	width: 50%;
+	min-width: 425px;
+	flex-grow: 0;
+	flex-shrink: 0;
+	padding: 10px;
+	box-sizing: border-box;
 
-		.shortcut-section-item {
-			width: 100%;
-			display: grid;
-			grid-template-columns: 33% 67%;
-			column-gap: 10px;
+	.shortcut-section-item {
+		width: 100%;
+		display: grid;
+		grid-template-columns: 33% 67%;
+		column-gap: 10px;
 
-			&:not(:first-child) {
-				margin-top: 10px;
-			}
+		&:not(:first-child) {
+			margin-top: 10px;
+		}
 
-			&__keys {
-				display: block;
-				text-align: end;
-			}
+		&__keys {
+			display: block;
+			text-align: end;
+		}
 
-			&__label {
-				display: block;
-				text-align: start;
-				padding-top: 5px;
-			}
+		&__label {
+			display: block;
+			text-align: start;
+			padding-top: 5px;
+		}
 
-			&__spacer {
-				margin: 0 3px;
-			}
+		&__spacer {
+			margin: 0 3px;
 		}
 	}
 }
+
 </style>
