46bd227e431b0cea462d597514bee1fbd0ff0d76
Richard Steinmetz <richard@steinmetz.cloud>
2025-09-25 11:33:35 +0200

    chore(eslint): forbid mixing tabs and spaces
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: 2018 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
 module.exports = {
 	extends: [
 		'@nextcloud/eslint-config/typescript',
@@ -11,4 +12,10 @@ module.exports = {
 			typescript: {},
 		},
 	},
+	plugins: [
+		'@stylistic',
+	],
+	rules: {
+		'@stylistic/no-mixed-spaces-and-tabs': 'error',
+	}
 }
