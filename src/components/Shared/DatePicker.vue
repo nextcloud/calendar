fc0a0a4306cb656d5cad776ba7ef96a42e122504
Maximilian Martin <maximilian_martin@gmx.de>
2025-10-03 07:29:44 +0000

    fix: prevent event date change glitch
    Signed-off-by: Maximilian Martin <maximilian_martin@gmx.de>
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- src/components/Shared/DatePicker.vue
+++ src/components/Shared/DatePicker.vue
@@ -12,14 +12,14 @@
 		:type="type"
 		:hide-label="true"
 		class="date-time-picker"
-		@input="change" />
+		@blur="onBlur"
+		@input="onInput" />
 </template>
 
 <script>
 import {
 	NcDateTimePickerNative as DateTimePicker,
 } from '@nextcloud/vue'
-import debounce from 'debounce'
 import { mapStores } from 'pinia'
 import useDavRestrictionsStore from '../../store/davRestrictions.js'
 
@@ -56,6 +56,12 @@ export default {
 		},
 	},
 
+	data() {
+		return {
+			pendingDate: null,
+		}
+	},
+
 	computed: {
 		...mapStores(useDavRestrictionsStore),
 		/**
@@ -83,13 +89,26 @@ export default {
 		 *
 		 * @param {Date} date The new Date object
 		 */
-		change: debounce(async function(date) {
+		onInput(date) {
+			// Buffer the input; only emit when the user leaves the field
 			if (this.disabledDate(date)) {
 				return
 			}
+			this.pendingDate = date
+		},
 
-			this.$emit('change', date)
-		}, 1000),
+		onBlur(event) {
+			// When focus leaves the picker, commit the pending date
+			if (this.pendingDate === undefined || this.pendingDate === null) {
+				return
+			}
+			const pending = this.pendingDate
+			this.pendingDate = null
+			if (this.disabledDate(pending)) {
+				return
+			}
+			this.$emit('change', pending)
+		},
 
 		/**
 		 * Whether or not the date is acceptable
