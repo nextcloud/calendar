c46aff61f5e5172962d2bf854be6abc92e4c53a8
SebastianKrupinski <krupinskis05@gmail.com>
2025-11-04 11:10:49 -0500

    chore: add aliases to vitest
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- vitest.config.js
+++ vitest.config.js
@@ -5,9 +5,15 @@
 
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue2'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
 	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		},
+	},
 	test: {
 		include: ['tests/javascript/unit/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
 		setupFiles: [
