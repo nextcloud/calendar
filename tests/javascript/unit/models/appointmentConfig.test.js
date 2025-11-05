998a5f685806c647f863d3b969e5765a51d37549
SebastianKrupinski <krupinskis05@gmail.com>
2025-11-05 07:15:18 -0500

    fix: appointment config tests
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>
    
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- tests/javascript/unit/models/appointmentConfig.test.js
+++ tests/javascript/unit/models/appointmentConfig.test.js
@@ -4,18 +4,27 @@
  */
 
 import AppointmentConfig from '../../../../src/models/appointmentConfig.js'
+import { getBaseUrl } from '@nextcloud/router'
 
 vi.mock('@nextcloud/calendar-availability-vue')
+vi.mock('@nextcloud/router', () => ({
+	generateUrl: vi.fn((url, params, options) => {
+		const baseURL = options?.baseURL || ''
+		let path = url
+		if (params) {
+			Object.keys(params).forEach(key => {
+				path = path.replace(`{${key}}`, params[key])
+			})
+		}
+		// When baseURL is provided, add /index.php prefix (mimics real Nextcloud router behavior)
+		return baseURL + (baseURL ? '/index.php' : '') + path
+	}),
+	getBaseUrl: vi.fn(),
+}))
 
 describe('models/appointmentConfig test suite', () => {
-	let windowSpy
-
-	beforeEach(() => {
-		windowSpy = vi.spyOn(window, 'window', 'get')
-	})
-
 	afterEach(() => {
-		windowSpy.mockRestore()
+		vi.clearAllMocks()
 	})
 
 	test.each([
@@ -36,10 +45,9 @@ describe('models/appointmentConfig test suite', () => {
 			'http://nextcloud.testing:8080/nextcloud/index.php/apps/calendar/appointment/foobar',
 		],
 	])('should generate absolute URLs', (location, expected) => {
-		windowSpy?.mockImplementation(() => ({
-			location,
-			_oc_webroot: '/nextcloud',
-		}))
+		const baseUrl = location.protocol + '//' + location.host + '/nextcloud'
+		getBaseUrl.mockReturnValue(baseUrl)
+		
 		const config = new AppointmentConfig({
 			token: 'foobar',
 		})
