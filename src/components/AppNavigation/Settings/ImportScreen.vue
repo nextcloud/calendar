19e1ee8c055bda5b936b9c13b218344d32215782
SebastianKrupinski <krupinskis05@gmail.com>
2025-12-03 16:51:18 -0500

    fixup! migrate to vue 3
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- src/components/AppNavigation/Settings/ImportScreen.vue
+++ src/components/AppNavigation/Settings/ImportScreen.vue
@@ -4,7 +4,12 @@
 -->
 
 <template>
-	<Modal class="import-modal" size="large" @close="cancelImport">
+	<Modal
+		class="import-modal"
+		size="large"
+		:name="$t('calendar', 'Import destination selection')"
+		@close="cancelImport">
+
 		<h2 class="import-modal__title">
 			{{ $t('calendar', 'Import calendars') }}
 		</h2>
@@ -14,7 +19,7 @@
 		</h4>
 
 		<transition-group class="import-modal__file-list" tag="ul">
-			<li :key="headerRowKey" class="import-modal-file-item import-modal-file-item--header">
+			<li key="import-header-row" class="import-modal-file-item import-modal-file-item--header">
 				<div class="import-modal-file-item__filename">
 					{{ $t('calendar', 'Filename') }}
 				</div>
@@ -52,12 +57,6 @@ export default {
 		files: {
 			type: Array,
 			required: true,
-		},
-	},
-
-	computed: {
-		headerRowKey() {
-			return this._uid + '-header-row'
 		},
 	},
 
