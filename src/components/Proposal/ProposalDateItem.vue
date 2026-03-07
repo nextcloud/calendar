b68a2f39611dd6170591e9ee839550d4f7847408
taiebot <dedreuil@yahoo.fr>
2026-03-07 22:31:17 +0100

    Refactor date formatting to use moment-timezone
    Will fix Summer time handling #8040 
    
    Signed-off-by: taiebot <dedreuil@yahoo.fr>

--- src/components/Proposal/ProposalDateItem.vue
+++ src/components/Proposal/ProposalDateItem.vue
@@ -56,17 +56,8 @@ export default {
 			if (!this.proposalDate.date) {
 				return ''
 			}
-			// Get the timezone offset in minutes
-			let timezoneOffset = 0
-			try {
-				const now = new Date()
-				const utcDate = new Date(now.toLocaleString('en-US', { timeZone: 'UTC' }))
-				const targetDate = new Date(now.toLocaleString('en-US', { timeZone: this.timezoneId }))
-				timezoneOffset = ((utcDate.getTime() - targetDate.getTime()) / (1000 * 60)) * -1
-			} catch (e) {
-				timezoneOffset = 0
-			}
-			const m = moment(this.proposalDate.date).utcOffset(timezoneOffset)
+			// Use moment-timezone to interpret the date in the correct IANA timezone
+			const m = moment.tz(this.proposalDate.date, this.timezoneId)
 			// Examples: "Mon, Jul 8, 2:30 PM" (en), "Mon, 8 Jul, 14:30" (en-GB), "Mo, 8. Jul, 14:30" (de)
 			return m.format('dddd, MMMM D, LT')
 		},
