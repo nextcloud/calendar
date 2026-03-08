5402ab0cc3023c05a493f566c3a172ebcf51f815
taiebot <dedreuil@yahoo.fr>
2026-03-08 17:37:32 +0100

    Refactor date formatting using formatInTimezone utility
    Signed-off-by: taiebot <dedreuil@yahoo.fr>

--- src/components/Proposal/ProposalDateItem.vue
+++ src/components/Proposal/ProposalDateItem.vue
@@ -25,6 +25,7 @@ import type { ProposalDateInterface } from '@/types/proposals/proposalInterfaces
 // types, object and stores
 import { t } from '@nextcloud/l10n'
 import moment from '@nextcloud/moment'
+import { formatInTimezone } from '@/utils/formatDate'
 // icons
 import ItemIcon from 'vue-material-design-icons/Calendar'
 import DestroyIcon from 'vue-material-design-icons/Close'
@@ -53,24 +54,9 @@ export default {
 
 	computed: {
 		formattedDate(): string {
-			if (!this.proposalDate.date) {
-				return ''
-			}
-			// Get the timezone offset in minutes
-			let timezoneOffset = 0
-			try {
-				const now = new Date()
-				const utcDate = new Date(now.toLocaleString('en-US', { timeZone: 'UTC' }))
-				const targetDate = new Date(now.toLocaleString('en-US', { timeZone: this.timezoneId }))
-				timezoneOffset = ((utcDate.getTime() - targetDate.getTime()) / (1000 * 60)) * -1
-			} catch (e) {
-				timezoneOffset = 0
-			}
-			const m = moment(this.proposalDate.date).utcOffset(timezoneOffset)
-			// Examples: "Mon, Jul 8, 2:30 PM" (en), "Mon, 8 Jul, 14:30" (en-GB), "Mo, 8. Jul, 14:30" (de)
-			return m.format('dddd, MMMM D, LT')
-		},
-	},
+			return formatInTimezone(this.proposalDate.date, this.timezoneId)
+        },
+    },
 
 	methods: {
 		t,
