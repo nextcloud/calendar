0a283a900bffe0eb61866970e01730848a1863ce
taiebot <dedreuil@yahoo.fr>
2026-03-21 21:59:19 +0100

    Refactor getTimezoneOffset to remove type annotations
    Remove TypeScript type annotations from getTimezoneOffset function.
    
    Signed-off-by: taiebot <dedreuil@yahoo.fr>

--- src/services/timezoneOffsetService.js
+++ src/services/timezoneOffsetService.js
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-export function getTimezoneOffset(proposalDate: string, timezoneId: string): number {
+export function getTimezoneOffset(proposalDate, timezoneId){
   let timezoneOffset = 0
 
   try {
