8386c750c928fdbf2a404efc7252fbbcbea763b9
Jonas <jonas@freesources.org>
2026-03-23 14:57:07 +0100

    fix(reference): migrate to Vue 3
    The reference provider widget registration was not ported to Vue 3 yet.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/reference.js
+++ src/reference.js
@@ -1,10 +1,11 @@
-import { getRequestToken } from '@nextcloud/auth'
-import { translate, translatePlural } from '@nextcloud/l10n'
-import { linkTo } from '@nextcloud/router'
 /**
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
+import { getRequestToken } from '@nextcloud/auth'
+import { translate, translatePlural } from '@nextcloud/l10n'
+import { linkTo } from '@nextcloud/router'
 import { NcCustomPickerRenderResult, registerWidget } from '@nextcloud/vue/functions/registerReference'
 
 import '../css/calendar.scss'
@@ -12,30 +13,26 @@ import '../css/calendar.scss'
 __webpack_nonce__ = btoa(getRequestToken())
 __webpack_public_path__ = linkTo('calendar', 'js/') // eslint-disable-line
 
-// eslint-disable-next-line no-unused-vars
-registerWidget('calendar_widget', async (el, { richObjectType, richObject, accessible, interactive }) => {
-	const { default: Vue } = await import('vue')
+registerWidget('calendar_widget', async (el, { richObject }) => {
+	const { createApp } = await import('vue')
 	const { default: Calendar } = await import('./views/Calendar.vue')
-	const { createPinia, PiniaVuePlugin } = await import('pinia')
+	const { createPinia } = await import('pinia')
 
-	Vue.use(PiniaVuePlugin)
 	const pinia = createPinia()
 
-	Vue.prototype.$t = translate
-	Vue.prototype.$n = translatePlural
-	Vue.mixin({ methods: { t, n } })
-
-	const Widget = Vue.extend(Calendar)
-	const vueElement = new Widget({
-		pinia,
-		propsData: {
-			isWidget: true,
-			isPublic: richObject.isPublic,
-			referenceToken: richObject?.token,
-			url: richObject.url,
-		},
-	}).$mount(el)
-	return new NcCustomPickerRenderResult(vueElement.$el, vueElement)
+	const app = createApp(Calendar, {
+		isWidget: true,
+		isPublic: richObject.isPublic,
+		referenceToken: richObject?.token,
+		url: richObject.url,
+	})
+	app.config.globalProperties.$t = translate
+	app.config.globalProperties.$n = translatePlural
+	app.config.globalProperties.t = translate
+	app.config.globalProperties.n = translatePlural
+	app.use(pinia)
+	app.mount(el)
+	return new NcCustomPickerRenderResult(el, app)
 }, (el, renderResult) => {
 	renderResult.object.$destroy()
-}, true)
+})
