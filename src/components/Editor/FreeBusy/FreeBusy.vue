f15264e612e8e141d40053dafe0c0498673f5813
Oleksandr Dzhychko <hey@oleks.dev>
2026-07-21 09:56:32 +0000

    fix(editor): using clear button breaks date picker in free busy dialog
    Signed-off-by: Oleksandr Dzhychko <hey@oleks.dev>

--- src/components/Editor/FreeBusy/FreeBusy.vue
+++ src/components/Editor/FreeBusy/FreeBusy.vue
@@ -665,6 +665,10 @@ export default {
 					calendar.next()
 					break
 				case 'picker':
+					// `date` is `null` when the "clear" button of the native date input was used.
+					if (date === null) {
+						return
+					}
 					calendar.gotoDate(date)
 					break
 			}
