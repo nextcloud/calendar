77a8c63d248b56d5cd97d0930a93b372618cb505
Oleksandr Dzhychko <hey@oleks.dev>
2026-07-22 17:19:59 +0000

    fix(editor): time input for relative reminders to all day events
    Signed-off-by: Oleksandr Dzhychko <hey@oleks.dev>

--- src/components/Shared/TimePicker.vue
+++ src/components/Shared/TimePicker.vue
@@ -5,11 +5,11 @@
 
 <template>
 	<DateTimePicker
-		:value="date"
+		v-model="internalValue"
 		type="time"
 		:hideLabel="true"
 		v-bind="$attrs"
-		@input="change" />
+		@blur="restoreClearedInput" />
 </template>
 
 <script>
@@ -32,17 +32,43 @@ export default {
 
 	data() {
 		return {
+			cleared: false,
 		}
+	},
+
+	computed: {
+		/**
+		 * Value displayed and manipulated by the wrapped NcDateTimePickerNative.
+		 * Coordinates passing the `date` and emitting changes to it
+		 * while tracking an intermediate state where NcDateTimePickerNative is cleared.
+		 */
+		internalValue: {
+			get() {
+				if (this.cleared) {
+					return null
+				}
+				return this.date
+			},
+
+			set(newValue) {
+				if (newValue === null) {
+					this.cleared = true
+					return
+				}
+				this.cleared = false
+				if (this.date.getHours() !== newValue.getHours() || this.date.getMinutes() !== newValue.getMinutes()) {
+					this.$emit('change', newValue)
+				}
+			},
+		},
 	},
 
 	methods: {
 		/**
-		 * Emits a change event for the Date
-		 *
-		 * @param {Date} date The new Date object
+		 * Restores last valid date into a input, if input is cleared.
 		 */
-		change(date) {
-			this.$emit('change', date)
+		restoreClearedInput() {
+			this.cleared = false
 		},
 	},
 }
