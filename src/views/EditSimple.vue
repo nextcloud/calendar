238339a2e63bd9f1c5e50d51ef423f88af0a6d40
Roberto Guido <info@madbob.org>
2026-09-07 18:23:49 +0200

    fix: hide invitation reply buttons on cancelled event
    Signed-off-by: Roberto Guido <info@madbob.org>

--- src/views/EditSimple.vue
+++ src/views/EditSimple.vue
@@ -221,7 +221,7 @@
 							:calendar="selectedCalendar" />
 
 						<InvitationResponseButtons
-							v-if="isViewedByAttendee && isViewing"
+							v-if="isViewedByAttendee && isViewing && !isCancelled"
 							class="event-popover__response-buttons"
 							:attendee="userAsAttendee"
 							@close="closeEditorAndSkipAction" />
@@ -375,7 +375,6 @@ export default {
 			boundaryElement: null,
 			isVisible: true,
 			isViewing: true,
-			isCancelled: false,
 			closeMask: false,
 			showCancelDialog: false,
 			cancelButtons: [
@@ -464,7 +463,6 @@ export default {
 			this.hasDescription = false
 			this.hasAttendees = false
 			this.hasAlarms = false
-			this.isCancelled = false
 
 			if (this.calendarObjectInstance) {
 				if (typeof this.calendarObjectInstance.location === 'string' && this.calendarObjectInstance.location.trim() !== '') {
@@ -478,9 +476,6 @@ export default {
 				}
 				if (Array.isArray(this.calendarObjectInstance.alarms) && this.calendarObjectInstance.alarms.length > 0) {
 					this.hasAlarms = true
-				}
-				if (this.calendarObjectInstance.status === 'CANCELLED') {
-					this.isCancelled = true
 				}
 
 				// Reposition after content changes
